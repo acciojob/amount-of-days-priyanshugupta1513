f4e79bd951836ae6cb896f0c22384cda3f9dde33
priyanshugupta1513 <priyanshu35828@gmail.com>
2025-05-31 18:38:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function getDays(year){
+	if(year%400==0 || (year%4==0 && year%100 !==0)){
+		return 366 ;
+	}else{
+		 return 365 ;
+	}
+}
+let year = prompt("enter valid year: ") 
+alert(getDays(year));
